4f0df68a2bcbf2657bb723d42770d84383b9bdb2
SonicCodes <seidramimohammed@gmail.com>
2024-08-02 04:39:40 +0300

    Update pretrain_llama_7b.sh

--- examples/pretrain_llama_7b.sh
+++ examples/pretrain_llama_7b.sh
@@ -1,20 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+# Optimized script to address training instability
 
-# This is the example script to pretrain a 7B LLaMA model on a TPU v4-512 pod.
-# These hyperparameters are the ones we used to train the OpenLLaMA 7B model on
-# the RedPajama dataset. To use this on TPU pod, you need to run this
-# script on every hosts in a TPU pod.
-
-# Put your WANDB API key here to enable logging to wandb.
-# export WANDB_API_KEY='<your wandb api key here>'
-
-# TPU specific flags to improve training throughput
 export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_enable_async_all_gather=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
-
 
 python -m EasyLM.models.llama.llama_train \
     --mesh_dim='-1,4,1' \
-    --dtype='fp32' \
+    --dtype='bfloat16' \
     --total_steps=100000 \
     --log_freq=10 \
     --save_model_freq=0 \
@@ -25,16 +16,19 @@ python -m EasyLM.models.llama.llama_train \
     --load_checkpoint='' \
     --optimizer.type='adamw' \
     --optimizer.adamw_optimizer.weight_decay=0.1 \
-    --optimizer.adamw_optimizer.lr=3e-4 \
-    --optimizer.adamw_optimizer.end_lr=3e-5 \
+    --optimizer.adamw_optimizer.init_lr=1e-6 \
+    --optimizer.adamw_optimizer.lr=1e-4 \
+    --optimizer.adamw_optimizer.end_lr=1e-5 \
     --optimizer.adamw_optimizer.lr_warmup_steps=2000 \
     --optimizer.adamw_optimizer.lr_decay_steps=250000 \
+    --optimizer.adamw_optimizer.clip_gradient=1.0 \
+    --optimizer.accumulate_gradient_steps=4 \
     --train_dataset.type='huggingface' \
     --train_dataset.text_processor.fields='text' \
     --train_dataset.huggingface_dataset.path='HuggingFaceFW/fineweb-edu' \
     --train_dataset.huggingface_dataset.streaming=True \
     --train_dataset.huggingface_dataset.seq_length=2048 \
-    --train_dataset.huggingface_dataset.batch_size=128 \
+    --train_dataset.huggingface_dataset.batch_size=32 \
     --train_dataset.huggingface_dataset.split='train' \
     --train_dataset.huggingface_dataset.name='sample-100BT' \
     --checkpointer.save_optimizer_state=True \
@@ -44,4 +38,3 @@ python -m EasyLM.models.llama.llama_train \
     --logger.output_dir="$HOME/experiment_output/llama3-log" \
     --logger.wandb_dir="$HOME/experiment_output/open_llama_3b" \
 |& tee $HOME/output.txt
-
